1db38264b4b9d36363107b0d065eb7688ec5065b
Thomas <georgemik@gmail.com>
2025-11-13 10:22:18 +0530

    second commit

--- provider.tf
+++ provider.tf
@@ -12,6 +12,6 @@ provider "azurerm" {
   subscription_id = "d2b83b21-b63f-4c16-a9c4-ee3d9553e497"
   client_id       = "be026083-3982-43ca-a2f5-0bf66f24d011"
     tenant_id       = "bbb8e3ab-7408-4c6f-a4ab-d87cf9ed5fc8"
-client_secret   = "iKG8Q~5ObS53eOUta54uDPUu_5fxRJfiE8x-ebl9"
+
 }
 
